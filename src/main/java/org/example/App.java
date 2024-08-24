05ff0f977a56abee17d6b949f63437aaeb39d462
amirdi <amir.dirin@yahoo.com>
2024-08-24 12:24:23 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+    public int add( int a, int b ) {
+
+        return a + b;
+    }
+}
